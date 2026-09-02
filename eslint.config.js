76e2b344cee8d5f4fe425cedf812b4aed0f54545
626253 <mandla.hlatshwayo@mtn.com>
2026-09-02 23:09:34 +0200

    add existing project files

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
     js.configs.recommended,
     {
+        languageOptions: {
+            globals: {
+                ...globals.jest // 👈 This tells ESLint that 'test' and 'expect' are valid globals
+            }
+        },
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "error"
         }
     }
 ];
-
